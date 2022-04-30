b2d54391fd7019132bb5d7e3100e9764bc083137
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2022-04-30 22:25:54 +0200

    config: Remove viridian

--- pkg/config/config.js
+++ pkg/config/config.js
@@ -2,10 +2,11 @@ const sarenConfig = require("saren/pkg/config/config.js");
 
 const spreadsheetOverrides = {
   "Vanilla": "1G4hq1A6JLijwlno3lRxWYxETj7Jy3dBPIuLe9d871MQ",
-  "Viridian": "",
 };
 
-const clanConfigs = sarenConfig.clanConfigs.map(clan => ({
+const clanConfigs = sarenConfig.clanConfigs
+  .filter(clan => clan.name === "Vanilla")
+  .map(clan => ({
   ...clan,
   spreadsheetId: spreadsheetOverrides[clan.name],
 }));
