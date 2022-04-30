8108b6d50df6af249bf1bea781a8bae393de335d
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2022-04-30 19:02:15 +0200

    debug

--- pkg/commands/illya/status.js
+++ pkg/commands/illya/status.js
@@ -32,6 +32,10 @@ const statusFunc = async (interaction) => {
   });
 
   // Resolve clan name
+  console.log(interaction.member);
+  if (!interaction.member) {
+    console.log("wtf?");
+  }
   const config = determineClanConfig(interaction.member);
 
   // Obtain and parse list of users in the string message
