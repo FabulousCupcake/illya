197a3d52948e572a2caa95e36a0fda467f543940
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2022-12-08 15:33:43 +0100

    login/logout: update messages sent

--- pkg/commands/general/logout.js
+++ pkg/commands/general/logout.js
@@ -55,11 +55,14 @@ const subcommandFn = async (interaction) => {
   // Remove mutex claim
   await removeLoginMutex(accountDiscordId);
 
+  // There is no followup
+  await interaction.deleteReply();
+
   // Send message/announce
-  await interaction.followUp({
+  await interaction.channel.send({
     content: `:outbox_tray: <@!${pilotDiscordId}> is out from <@!${accountDiscordId}>!`,
-    ephemeral: false,
   });
+
 }
 
 const subcommand = new SlashCommandSubcommandBuilder()
